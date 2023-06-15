72df69fd34b5c7a6c8ff45950a8926b55e4f9872
RomaGlu <roma.glu.69@gmail.com>
2023-06-15 23:38:31 +0300

    add shadow to smallView

--- BidgetTrackerMDF/Sources/Onboarding/ViewController.swift
+++ BidgetTrackerMDF/Sources/Onboarding/ViewController.swift
@@ -40,6 +40,12 @@ class ViewController: UIViewController {
         let smallView = UIView()
         smallView.layer.cornerRadius = 20
         smallView.backgroundColor = .white
+        smallView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.07).cgColor
+        smallView.layer.shadowOpacity = 1
+        smallView.layer.shadowRadius = 36
+        smallView.layer.shouldRasterize = true
+        smallView.layer.rasterizationScale = UIScreen.main.scale
+        smallView.layer.shadowOffset = CGSize(width: 0, height: 12)
         return smallView
     }()
     
